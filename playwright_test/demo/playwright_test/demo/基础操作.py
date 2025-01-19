dbdd4102cd61aab3ed9ace44caa7fa3fc47cddbb
dx <983643937@qq.com>
2025-01-19 17:49:28 +0800

    playwright 操作

--- playwright_test/demo/playwright_test/demo/基础操作.py
+++ playwright_test/demo/playwright_test/demo/基础操作.py
@@ -1,3 +1,5 @@
+
+
 from playwright.sync_api import sync_playwright
 import tkinter as tk
 
@@ -9,6 +11,7 @@ playwright_test 运行浏览器准备
 
 
 def get_screen_resolution():
+    """获取最大化浏览器的尺寸"""
     # 创建一个隐藏的根窗口
     root = tk.Tk()
     root.withdraw()  # 隐藏窗口
@@ -57,5 +60,54 @@ with sync_playwright() as p:
     # 多个元素执行js 代码
     page.locator('//h3//em').evaluate_all('elements => elements.forEach(element => element.innerText="black")')
     print('aa')
+    # 判断元素是否可见
+    visible=page.locator("元素").is_visible()
+    # 判断元素是否隐藏
+    visible=page.locator("元素").is_hidden()
+    # 元素高亮显示
+    page.locator("元素").highlight()
+    # 获取符合条件的所有元素
+    los=page.locator("元素").all()
+    # 获取符合条件的第一个定位元素对象
+    lo1 = page.locator("元素").first
+    print("元素定位：", lo1, "元素文本：", lo1.inner_text())
+    # 获取符合条件的最后一个定位元素对象
+    lo2 = page.locator("元素").last
+    # 通过索引获取指定位置定位元素对象
+    lon=page.locator("元素").nth(1)
+
+    """获取页面数据"""
+    # 统计元素在页面中的个数
+    res=page.locator("元素").count()
+    # 获取元素标签的属性值
+    res= page.locator("元素").get_attribute("标签属性")
+    # 获取元素的文本
+    page.locator("元素").inner_text()
+    # 获取input 元素输入框的value值
+    page.locator("元素").input_value()
+    # 获取匹配元素的所有值
+    value=page.locator("元素").all_inner_texts()
+    # 获取元素的坐标
+    res=page.locator("元素").bounding_box()
+
+    """获取元素的等待机制"""
+    # playwright 自带智能等待机制，默认为30秒，单位以毫秒计算
+    # 更改默认等待时间
+    page.set_default_timeout(1000)
+    # 等待页面的某个状态加载完成 参数：（domcontentloaded:dom 树，load:元素，networkidle:网络）
+    page.wait_for_load_state('load')
+    # 等待元素出现
+    page.wait_for_selector("元素")
+    # 通过状态等待元素消失
+    page.wait_for_selector("元素",state='hidden')
+    # 等待js 函数执行结束
+    page.wait_for_function("js 语法")
+    # 等待页面跳转到某个url(判断页面有没有跳转)
+    page.wait_for_url("https://www.baidu.com")
+    # 等待事件，处理函数
+    page.wait_for_event('download',lambda x:x+1)
     # 等待时间
     page.wait_for_timeout(10000)
+
+
+
